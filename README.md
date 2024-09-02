29132430c6210388c020e2b2cdd93bf2ae2638a4
Sai Samyuktha N <73626290+Sai-Sam-N@users.noreply.github.com>
2024-09-02 15:49:43 +0530

    completed tutorial video.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Refer video : https://youtu.be/W6NZfCO5SIk?si=u5_48LvS633fvpUs
+<br>
 1. The script element
    - The script element, aka the JS Code - can be added in the head or right before the end of the body tag.
    - Best practice is to add the script element right before the end of the body tag.
@@ -14,4 +16,29 @@
   - Inline JS Code : writing the JS code within the script tag, inside the html file.
   - Separation of Content: HTML is all about content, JS is all about behavior. 
     
+3. Node is a program that includes google's V8 JS engine. It's a runtime environment for executing JS code. 
+   Example: <br>
+   Run the JS file on cmd and it'll produce the output in cmd. <br>
+   Syntax : `node <js_filename>`
   
+4. Variables : used to store data temporarily (in Programming) <br>
+  Previously in the old days before ES6, we use the VAR keyword to declare a variable. But there are issues with VAR keyword. <br>
+  Best practice is to use LET (as of now). <br>
+
+5. Constants: Value once stored, cannot be changed. If reassignment is not allowed / not needed --> use `const`. Otherwise, use `let`.
+
+6. Types : types of values that can be assigned to the variables.
+  - Primitive types / Value types: Strings, Number, Boolean, Undefined, NULL
+  - Reference Types : Objects, Arrays, Function
+      - Objects : multiple related variables can be grouped as an object. Makes the code cleaner; Dpt and Bracket notation - Dot is used by convention. If property selected at runtime by the user, we go for Bracket notation. 
+      - Functions : a set of statements that performs a task or calculates a value. No semicolon for the function declaration. Parameter : variable at the time of declaration; Argment : Value passed during function call.
+      - <p style="color:red;">Template literals can be used to clean up the code</p>
+
+7. Dynamic Typing : One thing that separates JS from other programming types is that JS is a dynamic language. <br>
+   Types of Languages: <br>
+   - Static (Statically-typed): When a variable is declared, the type of the variable is fixed and it can't be changed. 
+   - Dynamic (Dynamically-typed) : When a variable is declared, its type can be changed during run-time.
+
+8. Fun fact, in JS all numeric types come under 'number'. So, 30 is a number; 12.20 is also a number.
+
+9. "undefined" is also a type, but its also a value.
--- index.js
+++ index.js
@@ -1,2 +1,85 @@
 // This is my first JS code
-console.log('Hello World');
+// console.log('Hello World');
+
+// ----------------------------------------------------------- VARIABLES --------------------------------------------------------------------------------------------
+// Previously in the old days before ES6, we use the VAR keyword to declare a variable. But there are issues with VAR keyword. 
+// Best practice is to use LET (as of now)
+// let name = 'Sai'; // let <variable_name>. Default value : undefined
+// console.log(name);
+
+// RULES OF DECLARING A VARIABLE
+// Cannot be a reserved keyword
+// Should be meaningful and descriptive
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Are case-sensitive
+// Moder best practice is to declare variables line by line and not at once
+
+// let firstName = 'Sai', lastName;
+// console.log(firstName);
+
+
+// ----------------------------------------------------------- CONSTANTS --------------------------------------------------------------------------------------------
+// In the real world, there are instances where you don't want the values of your variables to change. Solution ---> Constants.
+// const interestRate = 0.3;
+// console.log(interestRate);
+
+
+// ----------------------------------------------------------- PRIMITIVE / VALUE TYPES --------------------------------------------------------------------------------------------
+// let name = 'Samira'; // String literal
+// let age = 21; // Number literal
+// let isApproved = true; // Boolean literal - true / false
+// let firstName = undefined; // default value 
+// let selectedColor = null;
+// In ES6, we also have another primitive type called 'symbol'
+
+
+// ----------------------------------------------------------- typeof KEYWORD --------------------------------------------------------------------------------------------
+// console.log(typeof name);
+
+
+// ----------------------------------------------------------- REFERENCE TYPE : OBJECT --------------------------------------------------------------------------------------------
+// let person = {
+//     name : 'Sai',
+//     age : 21
+// };
+// console.log(person);
+
+// Accessing properties of the object
+// 1) Dot Notation
+// person.name = 'Aquina';
+// console.log(person.name);
+// 2) Bracket Notation
+// person['name'] = 'Aurelia';
+// console.log(person['name']);
+
+
+// ----------------------------------------------------------- REFERENCE TYPE : ARRAYS (Data structure used to represent a list of items) --> TYPE : OBJECT --------------------------------------------------------------------------------------------
+// each element has an index which determines the position of the element in the array.
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green'; // Length of the array is dynamic.
+// console.log(selectedColors);
+// we can have a mix of element types within the array:
+// selectedColors[3] = 100;
+// console.log(selectedColors);
+
+//DOT NOTATON
+// console.log(selectedColors.length);
+
+
+// ----------------------------------------------------------- REFERENCE TYPE : FUNCTIONS --------------------------------------------------------------------------------------------
+// a set of statements that performs a task or calculates a value.
+
+// Performing a task
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+
+// ----------------------------------------------------------- TYPES OF FUNCTIONS --------------------------------------------------------------------------------------------
+// Calculating a value
+function square(number) {
+    return number*number;
+}
+
+console.log(square(2));
